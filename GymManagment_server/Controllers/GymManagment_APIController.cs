add3c790d8350efcad5f14dd38df205d6d5aea37
User <User@LAB2-10>
2024-10-07 15:15:20 +0300

    re

---
+++ GymManagment_server/Controllers/GymManagment_APIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GymManagment_server.Models;
+namespace GymManagment_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GymManagment_APIController : ControllerBase
+    {
+        //a variable to hold a reference to the db context!
+        private BenDBContext context;
+        //a variable that hold a reference to web hosting interface (that provide information like the folder on which the server runs etc...)
+        private IWebHostEnvironment webHostEnvironment;
+        //Use dependency injection to get the db context and web host into the constructor
+        public GymManagment_APIController(BenDBContext context, IWebHostEnvironment env)
+        {
+            this.context = context;
+            this.webHostEnvironment = env;
+        }
+    }
+}
--- GymManagment_server/Program.cs
+++ GymManagment_server/Program.cs
@@ -1,4 +1,5 @@
-
+using GymManagment_server.Models;
+using Microsoft.EntityFrameworkCore;
 namespace GymManagment_server
 {
     public class Program
@@ -10,11 +11,46 @@ namespace GymManagment_server
             // Add services to the container.
 
             builder.Services.AddControllers();
+            #region Add Database context to Dependency Injection
+            //Read connection string from app settings.json
+            string connectionString = builder.Configuration
+                .GetSection("ConnectionStrings")
+                .GetSection("GymManagment_server").Value;
+
+            //Add Database to dependency injection
+            builder.Services.AddDbContext<BenDBContext>(
+                    options => options.UseSqlServer(builder.Services.AddDbContext<BenDBContext>(
+                   options => options.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Initial Catalog = GymManagment_server; User ID = TaskAdminLogin; Password = Petel123; Trusted_Connection = True; MultipleActiveResultSets = true"));));
+
+            #endregion
+            #region for debugginh UI
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            #endregion
+
+            var app = builder.Build();
+
+            #region for debugginh UI
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            #endregion
+            #region Add Session
+            app.UseSession(); //In order to enable session management
+            #endregion 
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
